a2d3efb5ea9b18be3524568ba552e76da502db8f
PPPOMAAA <89136122+PPPOMAAA@users.noreply.github.com>
2023-07-18 20:17:57 +1000

    triangleS

--- GraphicEngine/Main.cpp
+++ GraphicEngine/Main.cpp
@@ -27,9 +27,14 @@ int main() {
 	}
 
 	GLfloat vertices[] = {
-	-0.5f, -0.5f, 0.0f,
-	 0.5f, -0.5f, 0.0f,
-	 0.0f,  0.5f, 0.0f
+	 0.5f,  0.5f, 0.0f,  // Верхний правый угол
+	 0.5f, -0.5f, 0.0f,  // Нижний правый угол
+	-0.5f, -0.5f, 0.0f,  // Нижний левый угол
+	-0.5f,  0.5f, 0.0f   // Верхний левый угол
+	};
+	GLuint indices[] = {  // Помните, что мы начинаем с 0!
+		0, 1, 3,   // Первый треугольник
+		1, 2, 3    // Второй треугольник
 	};
 
 	const char* vertexShaderSource =
@@ -70,16 +75,16 @@ int main() {
 		0, 1, 2
 	};
 
-	GLuint VAO = 0, VBA = 0, IBA = 0;
+	GLuint VAO = 0, VBA = 0, EBO;
 	glCreateVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBA);
-	glGenBuffers(1, &IBA);
+	glGenBuffers(1, &EBO);
 
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBA);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBA);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indes), indes, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
@@ -107,7 +112,7 @@ int main() {
 
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
 
 		window.display();
